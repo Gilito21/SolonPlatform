c4ff72e20e5c8a714566e2db669c11ae28745e9d
Gilito21 <164108913+Gilito21@users.noreply.github.com>
2025-03-01 19:35:27 +0000

    another try working

--- SolonMVP/client/src/components/ui/navigation-menu.tsx
+++ SolonMVP/client/src/components/ui/navigation-menu.tsx
@@ -12,7 +12,7 @@ const NavigationMenu = React.forwardRef<
   <NavigationMenuPrimitive.Root
     ref={ref}
     className={cn(
-      "relative z-10 flex max-w-max flex-1 items-center justify-center bg-background",
+      "relative z-10 flex max-w-max flex-1 items-center justify-center bg-[#1a1a1a]", // Updated to a specific dark gray color
       className
     )}
     {...props}
